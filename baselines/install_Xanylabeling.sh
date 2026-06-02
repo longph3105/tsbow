65d0fa0ffdd2174b1f91b37dee51da4032ca34d8
ngochdm <ngochdm@gmail.com>
2026-06-02 15:34:59 +0900

    update install_Xanylabeling.sh

--- baselines/install_Xanylabeling.sh
+++ baselines/install_Xanylabeling.sh
@@ -1,9 +1,8 @@
+eval "$(conda shell.bash hook)"
 
-echo "Create & activate Xanylabeling environment before installation"
+conda create -n Xanylabeling  python=3.9 -y
 
-echo "     create: conda create -n Xanylabeling  python=3.9 -y"
-
-echo "     activate: conda activate Xanylabeling"
+conda activate Xanylabeling
 
 # Install ONNX Runtime GPU (CUDA 12.x)
 pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/ 
