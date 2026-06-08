7dd6ecad562bbff699452a51eb0ad93182d00b29
ngochdm <ngochdm@gmail.com>
2026-06-08 16:05:09 +0900

    add run_Xanylabeling.sh in baselines

---
+++ baselines/run_Xanylabeling.sh
@@ -0,0 +1,11 @@
+# How to Run
+# bash run_Xanylabeling.sh
+
+eval "$(conda shell.bash hook)"
+
+conda activate Xanylabeling
+
+# update path to X-Anylabeling folder before running
+cd /path/to/repo/X-AnyLabeling/
+
+python anylabeling/app.py
